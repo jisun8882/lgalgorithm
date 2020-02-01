ae985d91723d663044b1cfb2a0f03f78bc020457
jisun <jisun931026@gmail.com>
2020-02-01 17:13:35 +0900

    20200201

---
+++ src/LeetCode/P0278.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+public class P0278 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 3;
+		System.out.println(firstBadVersion(n));
+	}
+	
+	public static int firstBadVersion(int n) {
+        
+        int left = 1;
+        int right = n;
+        
+        while(left<right){
+            int mid = left + (right - left) / 2;
+            if(isBadVersion(mid)){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        
+        return left;
+    }
+
+}
